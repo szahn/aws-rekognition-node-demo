9c34aa090426593519e70c3b3ae2ca724af400e7
szahn <stuart.zahn@gmail.com>
2017-08-17 22:42:30 -0700

    fix

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,7 +5,7 @@ $(function(){
     $.get("labels.json", function(data){
         var html = "";
         while (data.length){
-            var item = items.pop();
+            var item = data.pop();
             html += "<div class=\"thumbnail\"><p>" + item.labels.join(", ") + "</p><img class=\"photo\" alt=\"" + item.id + "\" src=\"photos/" + item.filename + "\"/></div>";
         }
         el.append(html);
